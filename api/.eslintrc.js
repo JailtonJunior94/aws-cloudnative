e9a330feca1512f1d6490c81f3588362d278122c
Jailton Júnior <jailton.junior94@outlook.com>
2019-10-20 16:05:05 -0300

    adicionado eslint.

---
+++ api/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    es6: true,
+    node: true
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+
+  rules: {
+  }
+}
